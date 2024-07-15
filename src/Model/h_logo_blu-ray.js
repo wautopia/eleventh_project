af708dc4c3be415cb440e6098da635a0b65204f7
wautopia <gameprogrammer55@gmail.com>
2024-07-15 20:03:49 +0900

    feature_0.1

--- src/Model/h_logo_blu-ray.js
+++ src/Model/h_logo_blu-ray.js
@@ -3,7 +3,8 @@ import logoBluray from '../Media/header/h_logo_blu-ray.png';
 function Bluray() {
     return (
         <>
-            <img src={logoBluray} className="position-absolute top-0 d-none d-xl-block " alt="" style={{ width: '71%',right:'2%' }}></img>
+            <img src={logoBluray} className="position-absolute top-0 end-0 d-none d-xxl-block " alt="" style={{ width: '73%', }}></img>
+            <img src={logoBluray} className="position-absolute top-0 d-none d-xl-block d-xxl-none" alt="" style={{ width: '71%',right:'2%' }}></img>
             <img src={logoBluray} className="position-absolute d-none d-md-block d-xl-none" alt="" style={{ width: '71%', top: '2%' ,right:'2%',}}></img>
             <img src={logoBluray} className="mt-3 position-relative d-block d-md-none col-12" alt="" ></img>
        </>
